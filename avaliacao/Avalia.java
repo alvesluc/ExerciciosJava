45db144e9751109480cdc4c98a76657bbbc0465a
Lucas Alves <42820357+alvesluk@users.noreply.github.com>
2019-04-11 11:50:03 -0300

    Questão 7

--- avaliacao/Avalia.java
+++ avaliacao/Avalia.java
@@ -4,37 +4,13 @@ public class Avalia {
 	
 	public static void main(String[] args) {
 			
-		Aluno lucas = new Aluno();
-		Aluno luan = new Aluno();
-		Aluno lael = new Aluno();
-		Aluno doug = new Aluno();
-		Aluno joao = new Aluno();
-		Aluno wesley = new Aluno();
-		
-		lucas.setNome("Lucas");
-		lucas.setNota1((float)10);
-		lucas.setNota2((float)9);
-		
-		luan.setNome("Luan");
-		luan.setNota1((float)6);
-		luan.setNota2((float)6);
-		
-		lael.setNome("Lael");
-		lael.setNota1((float)8);
-		lael.setNota2((float)9);
-		
-		doug.setNome("Doug");
-		doug.setNota1((float)7);
-		doug.setNota2((float)7);
-		
-		joao.setNome("João");
-		joao.setNota1((float)2);
-		joao.setNota2((float)3);
+		Aluno lucas = new Aluno("Lucas", 10, 9);
+		Aluno luan = new Aluno("Luan", 6, 6);
+		Aluno lael = new Aluno("Lael", 8, 9);
+		Aluno doug = new Aluno("Doug", 7, 7);
+		Aluno joao = new Aluno("Pedro", 2, 3);
+		Aluno wesley = new Aluno("Wesley", 6, 6);
 
-		wesley.setNome("Wesley");
-		wesley.setNota1((float)6);
-		wesley.setNota2((float)6);
-		
 		lucas.fazerMedia();
 		luan.fazerMedia();
 		lael.fazerMedia();
@@ -48,7 +24,7 @@ public class Avalia {
 		doug.avalia();
 		joao.avalia();
 		wesley.avalia();
-
+		
+		Aluno.informa();
 	}
-
 }
